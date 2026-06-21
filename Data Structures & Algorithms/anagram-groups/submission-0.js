09db40c1715b25f013fb1b31600bf3961e142766
Leonardo Theodoro <simplespinheiro@gmail.com>
2026-06-21 11:24:04 -0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const anagrams = new Map()
+
+        if(strs.length === 1) {
+            return [strs]
+        } 
+
+        for(let i = 0; i < strs.length; i ++) {
+            const sorted = strs[i].split("").sort().join("")
+
+            if(anagrams.has(sorted)) {
+                const arr = anagrams.get(sorted)
+                anagrams.set(sorted, [...arr, strs[i]])
+            } else {
+                anagrams.set(sorted, [strs[i]])
+            }
+        }
+
+        const result = []
+        for(const value of anagrams.values()) {
+            result.push(value)
+        }
+
+        return result
+    }
+}
